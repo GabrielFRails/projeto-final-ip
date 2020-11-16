ddbcb884adf131305a3cc705db1cff72dd7915ba
Gabriel Freitas <gabriel.estudy.reis@gmail.com>
2020-11-16 15:58:57 -0300

    att funções e cases
    as seguintes funções foram adicionadas: incrementar_mes, decrementar_mes, print_current_month.
    Além de concluir o case 1 e começar o 2.

--- assets/main.c
+++ assets/main.c
@@ -20,36 +20,55 @@ struct Lembrete lembrete;
 
 int ano_bissexto(int ano); //função para verificar se um ano é bissexto ou não
 int numero_dias(int mes, int ano); //função para ver quantos dias tem um mês
-void incrementar_mes(int *mm, int *yy); 
-void decrementar_mes(int *mm, int *yy);
-char *dia_semana(int dia);
+void incrementar_mes(int *mm, int *yy); //função para incrementar o valor de um mês 
+void decrementar_mes(int *mm, int *yy); //função para decrementar o valor de um mês
+void print_mes(int mm, int yy); //not done yet
+void print_current_month(int mm, int yy); //not done yet
+char *dia_semana(int dia); //função para retornar o dia da semana de determinado dia
 int dis_semana(int dd, int mm, int aa); //função para saber qual dia da semana é de acordo com a data do usuário
 
 int main(){
-    setlocale(LC_ALL, " "); //desse modo podemos imprimir caracteres especiais da língua sem problemas
+    setlocale(LC_ALL, "Portuguese"); //desse modo podemos imprimir caracteres especiais da língua sem problemas
     int entrada = 0;
     char us = 'a';
+    int ref = 0;
 
-    //while(1){
-        printf("Bem vindo ser vivo altamente evoluído ou não\nDigite uma tecla para continuarmos\n");
+    while(ref==0){
+        printf("Bem vindo ser vivo altamente evoluido ou nao\nDigite uma tecla para continuarmos\n");
         printf("1. Localizar um dia\n");
-        printf("2. Exibir todos os dias do mês\n");
-        printf("3. Adicionar lembrete\n");
+        printf("2. Exibir todos os dias do mes\n");
+        printf("3. Adicionar lembrete(s)\n");
         printf("4. Sair do Programa\n");
-        //scanf("%d", &entrada);
-        printf("dia da semana: %s\n", dia_semana(10));
-/*        switch(entrada){
+        scanf("%d", &entrada);
+        //printf("dia da semana: %s\n", dia_semana(10));
+        switch(entrada){
             case 1:
+                printf("Digite uma data (DD / MM / AAAA)\n");
+                scanf("%d%d%d%*c", &data.dd, &data.mm, &data.yy);
+                printf("dia: %d mes: %d e ano: %d\n", data.dd, data.mm, data.yy);
+                int temp = dis_semana(data.dd, data.mm, data.yy);
+                printf("Dia da semana correspondente: %s\n", dia_semana(temp));
+                printf("Precione qualquer tecla para continuar\n");
+                getchar();
+                printf("\n");
+                break;
             case 2:
+                printf("Digite o mes e o ano (MM/AAAA)");
+                scanf("%d%d%*c", &data.mm, &data.yy);
+                /*a lógica aqui é ter uma função que imprime o mês e verificar se o user quer imprimir
+                    mais meses - próx e anterior*/
+                while(us!='e'){
+                    
+                }
+
             case 3:
             case 4: 
-                exit(0);
+                ref = 1;
+            default:
+                printf("Insira uma entrada valida camarada\n");
         }
-    }*/
-    lembrete.dd = 31;
-    lembrete.mm = 8;
-    scanf("%[^\n]", lembrete.nota);
-    printf("dia: %d\nmes: %d\n e nota: %s\n", lembrete.dd, lembrete.mm, lembrete.nota);
+    }
+    printf("\nObrigado por usar o nosso programa - by: Gabriel & Tallya\n");
     return 0;
 }
 
@@ -101,4 +120,39 @@ int dis_semana(int dd, int mm, int aa){
     d = q%7;
 
     return d;
+}
+
+void incrementar_mes(int *mm, int *yy){
+    (*mm)++;
+    if(*mm>12){
+        (*yy)++;
+        *mm -= 12;
+    }
+}
+
+void decrementar_mes(int *mm, int *yy){
+    (*mm)--;
+    if(*mm<1){
+        (*yy)--;
+        *mm += 12;
+    }
+}
+
+void print_current_month(int mm, int yy){
+    printf("\n");
+    switch(mm){
+        case 1: printf("Janeiro");
+        case 2: printf("Fevereiro");
+        case 3: printf("Marco");
+        case 4: printf("Abril");
+        case 5: printf("Maio");
+        case 6: printf("Junho");
+        case 7: printf("Julho");
+        case 8: printf("Agosto");
+        case 9: printf("Setembro");
+        case 10: printf("Outubro");
+        case 11: printf("Novembro");
+        case 12: printf("Dezembro");
+    }
+    printf(" de %d\n", yy);
 }
