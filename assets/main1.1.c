a730104d15f4ddd2bbbe6306cf9f8836e141075e
Tallya Barbosa <68134534+tallya01@users.noreply.github.com>
2020-11-20 23:59:07 -0300

    Update main1.1.c
    Código adaptado para funcionar com a ncurses.h

--- assets/main1.1.c
+++ assets/main1.1.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <conio.h>
-//#include <windows.h>
 #include <locale.h>
+#include <ncurses.h>
 
 struct Data{
     int dd;
@@ -30,75 +29,96 @@ int dis_semana(int dd, int mm, int aa); //função para saber qual dia da semana
 
 int main(){
     setlocale(LC_ALL, "Portuguese"); //desse modo podemos imprimir caracteres especiais da língua sem problemas
+    initscr();
+    keypad(stdscr, TRUE);
+    start_color();
+    noecho();
+    cbreak();
+    curs_set(0);
+
     int entrada = 0;
     char us = 'a';
-    char alteraMes = 'z';
-    int ref = 0;
+    int alteraMes = 0;
+    int ref = 0, i = 0;
 
     while(ref==0){
-        system("clear");
-        printf("Bem vindo, ser vivo altamente evoluído (ou não) c:\nEscolha uma opção para continuarmos:\n");
-        printf("1. Localizar um dia\n");
-        printf("2. Exibir todos os dias do mês\n");
-        printf("3. Adicionar lembrete(s)\n");
-        printf("4. Visualizar lembrete(s)\n");
-        printf("5. Sair do Programa\n");
-        scanf("%d", &entrada);
+        wclear(stdscr);
+        mvaddstr(0,0,"Bem vindo, ser vivo altamente evoluído (ou não) c:");
+        mvaddstr(1,0,"Escolha uma opção para continuarmos:");
+        mvaddstr(2,0,"1. Localizar um dia");
+        mvaddstr(3,0,"2. Exibir todos os dias do mês");
+        mvaddstr(4,0,"3. Adicionar lembrete(s)");
+        mvaddstr(5,0,"4. Visualizar lembrete(s)");
+        mvaddstr(6,0,"5. Sair do Programa");
+        entrada = getch();
     
         switch(entrada){
-            case 1:
-                printf("Digite uma data (DD MM AAAA)\n");
-                scanf("%d%d%d%*c", &data.dd, &data.mm, &data.yy);
-                system("clear");
-                printf("Dia: %d Mês: %d e Ano: %d\n", data.dd, data.mm, data.yy);
+            case '1':
+                mvaddstr(8,0,"Digite uma data (DD MM AAAA)");
+                echo();
+                curs_set(1);
+                mvscanw(9,0,"%d %d %d", &data.dd, &data.mm, &data.yy);
+                noecho();
+                curs_set(0);
+                wclear(stdscr);
+                mvprintw(0,0,"Dia: %d Mês: %d e Ano: %d", data.dd, data.mm, data.yy);
                 int temp = dis_semana(data.dd, data.mm, data.yy);
-                printf("Dia da semana correspondente: %s\n", dia_semana(temp));
-                printf("Pressione qualquer tecla para continuar\n");
-                getchar();
+                mvprintw(1,0,"Dia da semana correspondente: %s", dia_semana(temp));
+                mvaddstr(3,0,"Pressione qualquer tecla para continuar");
+                getch();
                 break;
 
-            case 2:
-                system("clear");
-                printf("Digite o mês e o ano (MM AAAA)\n");
-                scanf("%d%d", &data.mm, &data.yy);
+            case '2':
+                mvaddstr(8,0,"Digite o mês e o ano (MM AAAA)");
+                echo();
+                curs_set(1);
+                mvscanw(9,0,"%d%d", &data.mm, &data.yy);
+                noecho();
+                curs_set(0);
                 while(data.mm<1||data.mm>12){
-                    system("clear");
-                    printf("Entrada inválida!\nDigite o mês e o ano:\n");
-                    scanf("%d%d", &data.mm, &data.yy);
+                    wclear(stdscr);
+                    mvaddstr(0,0,"Entrada inválida! Digite o mês e o ano:");
+                    echo();
+                    curs_set(1);
+                    mvscanw(1,0,"%d%d", &data.mm, &data.yy);
+                    noecho();
+                    curs_set(0);
                 }
-                system("clear");
+                wclear(stdscr);
                 print_current_month(data.mm,data.yy);
                 while(alteraMes!='s'){
                     printf("\n");
-                    printf("Digite \"a\" para ver o mês anterior\n");
-                    printf("Digite \"p\" para ver o próximo mês\n");
-                    printf("Digite \"s\" para voltar ao menu principal\n");
-                    scanf("%*c%c", &alteraMes);
+                    mvaddstr(10,0,"Seta para a esquerda para ver o mês anterior");
+                    mvaddstr(11,0,"Seta para a direita para ver o próximo mês");
+                    mvaddstr(12,0,"Digite \"s\" para voltar ao menu principal");
+                    alteraMes = getch();
                     switch(alteraMes){
-                        case 'a':
-                        case 'A': decrementar_mes(&data.mm, &data.yy);
-                                  system("clear");
+                        case KEY_LEFT: decrementar_mes(&data.mm, &data.yy);
+                                  wclear(stdscr);
                                   print_current_month(data.mm, data.yy);
                                   break;
-                        case 'p': 
-                        case 'P': incrementar_mes(&data.mm, &data.yy);
-                                  system("clear");
+                        case KEY_RIGHT: incrementar_mes(&data.mm, &data.yy);
+                                  wclear(stdscr);
                                   print_current_month(data.mm, data.yy);
                                   break;
-                        case 's':
-                        case 'S': break;
+                        case 's':          
+                        case 'S': alteraMes='s';
+                                  break;
                     }
                 }
                 break;
 
-            case 3:
-            case 4:
-            case 5: 
+            case '3':
+            case '4':
+            case '5': 
                 ref = 1;
         }
     }
     system("clear");
-    printf("\nObrigado por usar o nosso programa - by: Gabriel & Tallya\n");
+    mvaddstr(0,0,"Obrigado por usar o nosso programa - by: Gabriel & Tallya");
+    mvaddstr(2,0,"Pressione uma tecla para encerrar");
+    getch();
+    endwin();
     return 0;
 }
 
@@ -191,40 +211,39 @@ void print_current_month(int mm, int yy){
         }
     }
     printf("\n");
-    printf("Mês: %d  Ano: %d\n", mm, yy);
+    mvprintw(0,0,"Mês: %d  Ano: %d", mm, yy);
     switch(mm){
-        case 1: printf("Janeiro\n");
+        case 1: mvprintw(1,0,"Janeiro");
                 break;
-        case 2: printf("Fevereiro\n");
+        case 2: mvprintw(1,0,"Fevereiro");
                 break;
-        case 3: printf("Março\n");
+        case 3: mvprintw(1,0,"Março");
                 break;
-        case 4: printf("Abril\n");
+        case 4: mvprintw(1,0,"Abril");
                 break;
-        case 5: printf("Maio\n");
+        case 5: mvprintw(1,0,"Maio");
                 break;
-        case 6: printf("Junho\n");
+        case 6: mvprintw(1,0,"Junho");
                 break;
-        case 7: printf("Julho\n");
+        case 7: mvprintw(1,0,"Julho");
                 break;
-        case 8: printf("Agosto\n");
+        case 8: mvprintw(1,0,"Agosto");
                 break;
-        case 9: printf("Setembro\n");
+        case 9: mvprintw(1,0,"Setembro");
                 break;
-        case 10: printf("Outubro\n");
+        case 10: mvprintw(1,0,"Outubro");
                 break;
-        case 11: printf("Novembro\n");
+        case 11: mvprintw(1,0,"Novembro");
                 break;
-        case 12: printf("Dezembro\n");
+        case 12: mvprintw(1,0,"Dezembro");
                 break;
     }
-    printf("D  S  T  Q  Q  S  S\n");
+    mvprintw(2,0,"D  S  T  Q  Q  S  S");
     for(i=0;i<6;i++){
         for(j=0;j<7;j++){
-            if(mes[i][j]==0) printf("x  ");
-            else if(mes[i][j]<10) printf("%d  ", mes[i][j]);
-            else printf("%d ", mes[i][j]);
+            if(mes[i][j]==0) mvprintw(i+3,j*3,"x");
+            else if(mes[i][j]<10) mvprintw(i+3,j*3,"%d", mes[i][j]);
+            else mvprintw(i+3,j*3,"%d", mes[i][j]);
         }
-        printf("\n");
     }
 }
